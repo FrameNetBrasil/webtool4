9df0c0528cbb3ca64b5dfe7d384dfc9d3975f3a5
Ely Edison Matos <ely.matos@gmail.com>
2024-11-01 08:19:36 -0300

    dockerfile - alloweddocs

--- app/Repositories/Project.php
+++ app/Repositories/Project.php
@@ -17,17 +17,26 @@ class Project
         $idUser = AppService::getCurrentIdUser();
         $user = User::byId($idUser);
         if (User::isManager($user)) {
-            $criteria = Criteria::table("view_project_docs");
+            $criteria = Criteria::table("view_project_docs as pd")
+                ->where("idLanguage", AppService::getCurrentIdLanguage())
+                ->whereIn('projectName', $projects)
+                ->select("idCorpus","corpusName","idDocument","documentName")
+                ->orderBy("corpusName")
+                ->orderBy("documentName")
+                ->all();
         } else {
-            $criteria = Criteria::table("view_alloweddocs");
-            $criteria = $criteria->where("idUser", $idUser);
+            $criteria = Criteria::table("view_alloweddocs as ad")
+                ->join("view_project_docs as pd","pd.idCorpus","=","ad.idCorpus")
+                ->where("ad.idUser", $idUser)
+                ->where("ad.idLanguage", AppService::getCurrentIdLanguage())
+                ->where("pd.idLanguage", AppService::getCurrentIdLanguage())
+                ->whereIn('pd.projectName', $projects)
+                ->select("ad.idCorpus","ad.corpusName","ad.idDocument","ad.documentName")
+                ->orderBy("ad.corpusName")
+                ->orderBy("ad.documentName")
+                ->all();
         }
-        return $criteria->where("idLanguage", AppService::getCurrentIdLanguage())
-            ->whereIn('projectName', $projects)
-            ->select("idCorpus","corpusName","idDocument","documentName")
-            ->orderBy("corpusName")
-            ->orderBy("documentName")
-            ->all();
+        return $criteria;
     }
 
 
