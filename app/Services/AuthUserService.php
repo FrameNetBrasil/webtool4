e71f9e4f9b83c3cc7e9384a5a18d5dba2d6d51af
Ely Edison Matos <ely.matos@gmail.com>
2024-12-03 14:55:48 -0300

    Update AuthUserService.php

--- app/Services/AuthUserService.php
+++ app/Services/AuthUserService.php
@@ -71,7 +71,7 @@ class AuthUserService
                     }
                     session(['mail_token' => $token]);
                     session(['twofactor_iduser' => $user->idUser]);
-                    Mail::to("ely.matos@gmail.com")->send(new WebToolMail($token));
+                    Mail::to($user->email)->send(new WebToolMail($token));
                     return 'checked';
                 } else {
                     return 'failed';
