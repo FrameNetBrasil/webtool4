1fa1b4b9ee0b8cd9d6017e4b7d2e7d3842aa9e9c
Ely Edison Matos <ely.matos@gmail.com>
2025-01-15 16:35:12 -0300

    Testing media server

--- app/Data/Image/CreateData.php
+++ app/Data/Image/CreateData.php
@@ -25,49 +25,26 @@ class CreateData extends Data
         $this->idUser = AppService::getCurrentIdUser();
         $this->originalFile = $file->getClientOriginalName();
         $extension = $file->getClientOriginalExtension();
-        $fileName = $this->name;
-        //$file->storeAs('images', $fileName);
-        //$file->store($fileName, 'media');
 
         $client = new Client([
-            'base_uri' => "http://host.docker.internal:8020",
             'timeout' => 300.0,
         ]);
-
-        $image_path = $file->getPathname();
-//        $image_mime = $file->getMimeType();
-//        $image_org  = $file->getClientOriginalName();
-//
-//        $response = $client->post('/', [
-//            'multipart' => [
-//                [
-//                    'name'     => 'image',
-//                    'filename' => $image_org,
-//                    'Mime-Type'=> $image_mime,
-//                    'contents' => fopen( $image_path, 'r' ),
-//                ],
-//            ]
-//        ]);
-
-        debug($image_path);
-//        $body = Utils::tryFopen($image_path, 'r');
-//        $r = $client->request('POST', "http://host.docker.internal:8020", ['body' => $body]);
-
-        debug(Utils::tryFopen($image_path, 'r'));
-
-        $response = $client->request('POST', 'http://host.docker.internal:8020', [
+        $url = config('webtool.mediaURL');
+        $response = $client->request('POST', $url, [
             'multipart' => [
                 [
-                    'name'     => 'file_name',
-                    'contents' => Utils::tryFopen($image_path, 'r'),
-                    'filename' => 'filename.png',
+                    'name'     => 'file',
+                    'contents' => Utils::tryFopen($file->getPathname(), 'r'),
+                    'filename' => $this->originalFile,
                 ]
             ]
         ]);
+        $this->currentURL = trim(str_replace($url . '/', '',(string)$response->getBody()));
+
         $dimensions = $file->dimensions();
-        $this->width = $dimensions[0];
-        $this->height = $dimensions[1];
-        $this->depth = $dimensions['bits'];
+        $this->width = $dimensions[0] ?? 0;
+        $this->height = $dimensions[1] ?? 0;
+        $this->depth = $dimensions['bits'] ?? 0;
     }
 
 }
--- app/Data/Video/CreateData.php
+++ app/Data/Video/CreateData.php
@@ -3,6 +3,8 @@
 namespace App\Data\Video;
 
 use App\Services\AppService;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Utils;
 use Illuminate\Http\UploadedFile;
 use Spatie\LaravelData\Data;
 
@@ -10,6 +12,7 @@ class CreateData extends Data
 {
     public function __construct(
         public ?string       $title = '',
+        public ?string       $currentURL = '',
         public ?string       $sha1Name = '',
         public ?string       $originalFile = '',
         public ?UploadedFile $file = null,
@@ -19,11 +22,25 @@ class CreateData extends Data
     {
         $this->idUser = AppService::getCurrentIdUser();
         $this->originalFile = $file->getClientOriginalName();
-        $this->sha1Name = sha1($file->getClientOriginalName());
-        $extension = $file->getClientOriginalExtension();
-        $fileName = $this->sha1Name . '_original' . '.' . $extension;
-        $file->storeAs('videos', $fileName);
+//        $md5 = md5($file->getClientOriginalName());
+//        $extension = $file->getClientOriginalExtension();
+        $this->sha1Name = sha1($this->originalFile);
+        //$file->storeAs('videos', $fileName);
 
+        $client = new Client([
+            'timeout' => 300.0,
+        ]);
+        $url = config('webtool.mediaURL');
+        $response = $client->request('POST', $url, [
+            'multipart' => [
+                [
+                    'name'     => 'file',
+                    'contents' => Utils::tryFopen($file->getPathname(), 'r'),
+                    'filename' => $this->originalFile,
+                ]
+            ]
+        ]);
+        $this->currentURL = trim(str_replace($url . '/', '',(string)$response->getBody()));
     }
 
 }
