f5ed17a13e95798e694f4872daa8b45bf041f848
Ely Edison Matos <ely.matos@gmail.com>
2025-06-09 08:41:52 -0300

    Fix CE constraint morpheme

--- app/View/Components/Combobox/LexiconMorpheme.php
+++ app/View/Components/Combobox/LexiconMorpheme.php
@@ -3,6 +3,7 @@
 namespace App\View\Components\Combobox;
 
 use App\Repositories\Lexicon;
+use App\Services\AppService;
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
@@ -18,8 +19,12 @@ class LexiconMorpheme extends Component
         public string $placeholder = '',
         public string $name = '',
         public ?int $value = 0,
+        public ?int $idLanguage = 0,
     )
     {
+        if ($this->idLanguage == 0) {
+            $this->idLanguage = AppService::getCurrentIdLanguage();
+        }
         if (($this->value != '') && ($this->value != 0)) {
             $lexicon = Lexicon::byId($this->value);
             $this->placeholder = $lexicon->name;
